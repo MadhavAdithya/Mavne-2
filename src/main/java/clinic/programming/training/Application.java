4f9467694ab738973d42b2753a5c364f85b66846
Maddy <madhavadithya@ubuntu.com>
2025-10-15 12:20:02 +0000

    Adding Depencies for our project in pom file

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,14 +1,21 @@
 package clinic.programming.training;
-
+import org.apache.commons.lang3.StringUtils;
 public class Application {
-    
+
+    public int countWords(String words){
+        String[] seperateWords = StringUtils.split(words,' ');
+        return (seperateWords==null)?0:seperateWords.length;
+    }
+
     public Application() {
         System.out.println ("Inside Application");
     }
 
     // method main(): ALWAYS the APPLICATION entry point
     public static void main (String[] args) {
-    	System.out.println ("Starting Application");
-	Application app = new Application();
+        System.out.println ("Starting Application");
+        Application app = new Application();
+        int count = app.countWords("I Have four Words");
+        System.out.println("Words Count : " + count);
     }
 }
